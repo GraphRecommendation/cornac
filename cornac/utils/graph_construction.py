66e8c1db421372df5ab9179227599fda5cbd090f
tj45794 <tjendal@cs.aau.dk>
2023-01-08 17:16:08 +0800

    Script for extracting test results

--- cornac/utils/graph_construction.py
+++ cornac/utils/graph_construction.py
@@ -1,9 +1,13 @@
-def create_heterogeneous_graph(train_set):
+import numpy as np
+
+
+def create_heterogeneous_graph(train_set, bipartite=True):
     """
     Create a graph with users, items, aspects and opinions.
     Parameters
     ----------
     train_set : Dataset
+    bipartite: if false have a different edge type per rating; otherwise, only use interacted.
 
     Returns
     -------
@@ -30,10 +34,14 @@ def create_heterogeneous_graph(train_set):
     for indices in list(zip(*train_set.matrix.nonzero())):
         rating_types.add(train_set.matrix[indices])
 
-    train_types = []
-    for rt in rating_types:
-        edge_types[str(rt)] = []
-        train_types.append(str(rt))
+    if not bipartite:
+        train_types = []
+        for rt in rating_types:
+            edge_types[str(rt)] = []
+            train_types.append(str(rt))
+    else:
+        train_types = ['interacted']
+        edge_types['interacted'] = []
 
     sentiment_modality = train_set.sentiment
     n_users = len(train_set.uid_map)
@@ -52,30 +60,39 @@ def create_heterogeneous_graph(train_set):
                 oid += n_items + n_users + n_aspects
 
                 edge_types['mentions'].append([uid, aid])
+                edge_types['mentions'].append([uid, oid])
                 edge_types['described_as'].append([iid, aid])
-                edge_types['has_opinion'].append([iid, oid])
+                edge_types['described_as'].append([iid, oid])
                 edge_types['co-occur'].append([aid, oid])
 
-            edge_types[str(train_set.matrix[(org_uid, iid)])].append([uid, iid])
+            if not bipartite:
+                edge_types[str(train_set.matrix[(org_uid, iid)])].append([uid, iid])
+            else:
+                edge_types['interacted'].append([uid, iid])
 
     # Create reverse edges.
     reverse = {}
     for etype, edges in edge_types.items():
         reverse['r_' + etype] = [[t, h] for h, t in edges]
 
-    edge_types.update(reverse)
+    # edge_types.update(reverse)
     n_relations = len(edge_types)
     edges = [[h, t] for k in sorted(edge_types) for h, t in edge_types.get(k)]
-    edges_t = torch.LongTensor(edges).T
+    edges_t = torch.LongTensor(edges).unique(dim=0).T
 
-    id_et_map = {i: et for i, et in enumerate(sorted(edge_types))}
+    et_id_map = {et: i for i, et in enumerate(sorted(edge_types))}
 
-    g = dgl.graph((edges_t[0], edges_t[1]), num_nodes=n_nodes)
-    type_label = torch.cat([torch.LongTensor([(i, float(k) if k in train_types else 0) for _ in edge_types.get(k)])
-                                 for i, k in sorted(id_et_map.items())]).T
-    g.edata['type'] = type_label[0]
+    g = dgl.graph((torch.cat(edges_t[0], edges_t[1]), torch.cat(edges_t[1], edges_t[0])), num_nodes=n_nodes)
+    inverse_et = {v: k for k, l in edge_types.items() for v in l}
+    et = torch.LongTensor([et_id_map[inverse_et[v]] for v in edges_t.T.tolist()])
 
-    g.edata['label'] = type_label[1].to(torch.float)
+    # Return 0 if not a rating type, else if using actual ratings return values else return 1 (bipartite).
+    value_fn = lambda etype: 0 if etype not in train_types else (float(etype) if etype != 'interacted' else 1)
+    labels = torch.FloatTensor([value_fn(inverse_et[v]) for v in edges_t.T.tolist()])
+
+    g.edata['type'] = torch.cat([et, et + n_relations])
+
+    g.edata['label'] = torch.cat([labels, labels])
     g.edata['a'] = dgl.ops.edge_softmax(g, torch.ones_like(g.edata['label']))
 
-    return g, n_nodes, n_items, n_relations
+    return g, n_nodes, n_items, n_relations * 2
